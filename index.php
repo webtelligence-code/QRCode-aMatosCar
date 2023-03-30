3b3665dcafaeaad6ffba41a07d265825a090c48e
webtelligence-code <manuelbcarreiras@gmail.com>
2023-03-30 10:39:55 +0100

    Removed script import unnecessary

--- index.php
+++ index.php
@@ -163,6 +163,5 @@
 </body>
 <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
 <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="scripts/index.js"></script>
 
 </html>
